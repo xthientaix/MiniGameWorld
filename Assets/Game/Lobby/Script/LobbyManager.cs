2362e5defbb0b24f7c6afd964d5cd740b94659e8
xthientaix <xthientaix@gmail.com>
2026-01-04 19:40:04 +0700

    Update

--- Assets/Game/Lobby/Script/LobbyManager.cs
+++ Assets/Game/Lobby/Script/LobbyManager.cs
@@ -1,18 +1,43 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Lobby
 {
     public class LobbyManager : MonoBehaviour
     {
+        [SerializeField] Image blackScreen;
+        [SerializeField] float fadeDuration;
+
+        private void Start()
+        {
+            Time.timeScale = 1f;
+            FadeOut();
+        }
+
         public void LoadScene(int sceneIndex)
         {
-            SceneManager.LoadScene(sceneIndex);
+            blackScreen.gameObject.SetActive(true);
+            blackScreen.DOFade(1f, fadeDuration).OnComplete(() =>
+            {
+                SceneManager.LoadScene(sceneIndex);
+            });
         }
 
         public void QuitGame()
         {
             Application.Quit();
+        }
+
+        private void FadeOut()
+        {
+            blackScreen.gameObject.SetActive(true);
+            blackScreen.color = new Color(0, 0, 0, 1);
+            blackScreen.DOFade(0f, fadeDuration).OnComplete(() =>
+            {
+                blackScreen.gameObject.SetActive(false);
+            });
         }
     }
 }
